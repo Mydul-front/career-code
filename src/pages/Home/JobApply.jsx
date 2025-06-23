689e2186a8ebd3b0437eac1769c1ddcc6e40bdd8
Mydul Islam <si240147@gmail.com>
2025-06-23 17:11:22 +0600

    add to post

--- src/pages/Home/JobApply.jsx
+++ src/pages/Home/JobApply.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import React from "react";
+import { Link, useParams } from "react-router";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 const JobApply = () => {
-     return (
-          <div>
-               <h3>Apply Job for</h3>
-          </div>
-     );
+  const { id: jobId } = useParams();
+  const { user } = useAuth();
+  console.log(jobId, user);
+  const handleApplyFormSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
+    const gitHub = form.gitHub.value;
+    const resume = form.resume.value;
+    console.log(linkedIn, gitHub, resume);
+
+    const application = {
+      jobId,
+      applicant: user.email,
+      linkedIn,
+      gitHub,
+      resume,
+    };
+    axios
+      .post("http://localhost:5000/applications", application)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your application has been submitted",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  return (
+    <div>
+      <h3>
+        Apply for this job: <Link to={`/jobs/${jobId}`}>Details</Link>
+      </h3>
+      <form onSubmit={handleApplyFormSubmit}>
+        <fieldset className="fieldset bg-base-200 my-7 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Page details</legend>
+
+          <label className="label">LinkedIn Link</label>
+          <input
+            type="url"
+            name="linkedIn"
+            className="input"
+            placeholder="LinkedIn Link"
+          />
+
+          <label className="label">GitHub Link</label>
+          <input
+            type="url"
+            name="gitHub"
+            className="input"
+            placeholder="GitHub Link"
+          />
+
+          <label className="label">Resume Link</label>
+          <input
+            type="url"
+            name="resume"
+            className="input"
+            placeholder="Resume Link"
+          />
+
+          <input type="submit" className="btn" value="Apply" />
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default JobApply;
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -6,8 +6,8 @@ const PrivateRoute = ({ children }) => {
   const { user, loading } = use(AuthContext);
   const location = useLocation();
   console.log(location.pathname);
-  if(loading){
-     return <span className="loading loading-spinner loading-xl"></span>;
+  if (loading) {
+    return <span className="loading mx-auto loading-spinner loading-xl"></span>;
   }
   if (!user) {
     return <Navigate to="/login" state={location.pathname}></Navigate>;
